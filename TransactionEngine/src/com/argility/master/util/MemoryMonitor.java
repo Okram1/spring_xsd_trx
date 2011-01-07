b8be0a0bfe77942099e69cd39534136ab6c8ddde
marko.salic <marko.salic@ba86d586-86ba-40fc-8881-1e5eeabb6462>
2011-01-07 13:36:59 +0000

    added number of available processors to memory monitor
    git-svn-id: svn://localhost/cordys_retail_src@16 ba86d586-86ba-40fc-8881-1e5eeabb6462

--- TransactionEngine/src/com/argility/master/util/MemoryMonitor.java
+++ TransactionEngine/src/com/argility/master/util/MemoryMonitor.java
@@ -37,14 +37,16 @@ public class MemoryMonitor {
 	    long maxMemory = runtime.maxMemory();
 	    long allocatedMemory = runtime.totalMemory();
 	    long freeMemory = runtime.freeMemory();
+	    int availableProcessors = runtime.availableProcessors();
 
 	    sb.append("\n\n");
-	    sb.append("JVM MEMEORY INFORMATION:\n");
-	    sb.append("used memory: " + format.format((allocatedMemory - freeMemory) / 1024) + "\n");
-	    sb.append("free memory: " + format.format(freeMemory / 1024) + "\n");
-	    sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "\n");
-	    sb.append("max memory: " + format.format(maxMemory / 1024) + "\n");
-	    sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "\n");
+	    sb.append("JVM MEMORY INFORMATION:\n");
+	    sb.append("Used memory: " + format.format((allocatedMemory - freeMemory) / 1024) + "\n");
+	    sb.append("Free memory: " + format.format(freeMemory / 1024) + "\n");
+	    sb.append("Allocated memory: " + format.format(allocatedMemory / 1024) + "\n");
+	    sb.append("Max memory: " + format.format(maxMemory / 1024) + "\n");
+	    sb.append("Total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "\n");
+	    sb.append("Available processors: " + availableProcessors + "\n");
 	    
 	    log.info(sb.toString());
     }
